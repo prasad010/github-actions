6ee546a78e3c38bf60aacafc32ad02057a28a64c
Durga Prasad <thota.prasad@outlook.com>
2026-01-17 13:42:50 +0530

    added git push file

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,11 @@
-#!/bin/bash 
-git add .
-git commit -m $1
-echo $(git branch)
-git push -u github_actions main
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo $pwd 
+git add ./.github/workflows/*
+git commit -m "$1"
+echo -e ${GREEN} $(git branch)
+echo -e ${RED} $(git push -u github_actions main)
